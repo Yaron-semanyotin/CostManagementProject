36e64555521bbb9a574ce0c5d9f48c13efed3aca
Yaron Semanyotin <126324yaron@gmail.com>
2026-07-25 17:34:11 +0300

    DB connection test created at BLL and DAL folders

---
+++ CostWise/App_Code/BLL/DatabaseConnectionTestBLL.cs
@@ -0,0 +1,12 @@
+﻿using CostWise.App_Code.DAL;
+
+namespace CostWise.App_Code.BLL
+{
+    public static class DatabaseConnectionTestBLL
+    {
+        public static void TestConnection()
+        {
+            DatabaseConnectionTestDAL.TestConnection();
+        }
+    }
+}
---
+++ CostWise/App_Code/DAL/DatabaseConnectionTestDAL.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace CostWise.App_Code.DAL
+{
+    public static class DatabaseConnectionTestDAL
+    {
+        public static void TestConnection()
+        {
+            using (SqlConnection connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+            }
+        }
+    }
+}
