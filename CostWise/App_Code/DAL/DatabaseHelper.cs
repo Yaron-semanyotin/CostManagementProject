dcf0ffed4a8a04014adb3f55d6f41821d340bb36
Yaron Semanyotin <126324yaron@gmail.com>
2026-07-25 17:07:04 +0300

    DAL Layer and GetConnection function created

---
+++ CostWise/App_Code/DAL/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CostWise.App_Code.DAL
+{
+    public static class DatabaseHelper
+    {
+        private const string ConnectionStringName = "CostWiseConnectionString";
+        public static SqlConnection GetConnection()
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[ConnectionStringName];
+            if (settings == null || string.IsNullOrWhiteSpace(settings.ConnectionString))
+                throw new ConfigurationErrorsException("Missing connection string: " + ConnectionStringName);
+            return new SqlConnection(settings.ConnectionString);
+        }
+    }
+}
